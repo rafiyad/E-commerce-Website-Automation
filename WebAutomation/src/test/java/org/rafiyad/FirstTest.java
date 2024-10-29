01140777c79dfb474b6d9bb5b84eea62a8cfe2d2
Rafiyad_Darazat <rafiyaddarazat@gmail.com>
2024-10-29 22:06:54 +0600

    two testng function added

--- WebAutomation/src/test/java/org/rafiyad/FirstTest.java
+++ WebAutomation/src/test/java/org/rafiyad/FirstTest.java
@@ -2,22 +2,46 @@ package org.rafiyad;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FirstTest {
-    public static void main(String[] args) throws InterruptedException {
-        //open  a browser
-
-        //test page title should be "Buy Islamic Books - Online Book Shop in Bangladesh | Wafilife"
-
-        //close the browser
+    @Test
+    public void testPageTitle(){
         String targetTitle = "Buy Islamic Books - Online Book Shop in Bangladesh | Wafilife";
 
+        //Loading browser for work
         WebDriver browser =new ChromeDriver();
+
+        //Going to the target url
         browser.get("https://www.wafilife.com/");
-        String titleOfWebsite=browser.getTitle();
-        System.out.println(titleOfWebsite);
-        
+
+
+        //String titleOfWebsite=browser.getTitle();
+        //System.out.println(titleOfWebsite);
+
+        Assert.assertEquals(browser.getTitle(),targetTitle);
+
+        browser.close();
+
+    }
+
+    @Test
+    public void testUrl(){
+        String targetUrl = "https://www.wafilife.com/";
+
+        //Loading browser for work
+        WebDriver browser =new ChromeDriver();
+
+        //Going to the target url
+        browser.get("https://www.wafilife.com/");
+
+
+        //String titleOfWebsite=browser.getTitle();
+        //System.out.println(titleOfWebsite);
+
+        Assert.assertEquals(browser.getCurrentUrl(),targetUrl);
+
         browser.close();
 
     }
